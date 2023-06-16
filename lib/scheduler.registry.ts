e46905eac87f00a4c7a81e038e0346c252946fe5
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2023-06-16 17:07:52 -0400

    feat: remove deprecated `SchedulerRegistry#doesExists` method

--- lib/scheduler.registry.ts
+++ lib/scheduler.registry.ts
@@ -10,11 +10,6 @@ export class SchedulerRegistry {
   private readonly timeouts = new Map<string, any>();
   private readonly intervals = new Map<string, any>();
 
-  /**
-   * @deprecated Use the `doesExist` method instead.
-   */
-  // TODO(v2): drop this.
-  doesExists = this.doesExist;
   doesExist(type: 'cron' | 'timeout' | 'interval', name: string) {
     switch (type) {
       case 'cron':
